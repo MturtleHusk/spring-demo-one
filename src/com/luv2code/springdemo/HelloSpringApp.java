8e03401f60e22326508c2cdd6fb32c984a3e522e
Mitch Freedman <armamantarium1@gmail.com>
2018-09-17 18:38:20 -0400

    finieshed basic first spring application

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -1,16 +1,21 @@
 package com.luv2code.springdemo;
+// imported for ClassPathXmlApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
 		// load the spring config file
-		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml"); 
+				
 		// retrieve bean from spring container
+		Coach theCoach = context.getBean(/*bean id*/ "myCoach", /*interface*/ Coach.class);
 		
 		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
 		
-		// close the context		
-
+		// close the context	
+		context.close();
 	}
 
 }
