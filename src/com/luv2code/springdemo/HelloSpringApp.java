ecb5400fa57fe1cfbf17947b4d84e75930d3642d
Mitch Freedman <armamantarium1@gmail.com>
2018-09-17 18:32:08 -0400

    actually added the helloSpringApp

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -3,7 +3,13 @@ package com.luv2code.springdemo;
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// load the spring config file
+		
+		// retrieve bean from spring container
+		
+		// call methods on the bean
+		
+		// close the context		
 
 	}
 
